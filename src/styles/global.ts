b22a805657b79da752a89f53883769e4f6832d97
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-17 16:08:35 -0300

    style: add stitches + globalcss + eslint

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from "./stitches.config";
+
+export const globalStyles = globalCss({
+  "*": {
+    boxSizing: "border-box",
+    padding: 0,
+    margin: 0,
+  },
+
+  body: {
+    backgroundColor: "$gray800",
+    color: "$gray100",
+    "-webkit-font-smoothing": "antialiased",
+  },
+
+  "body, input, textarea, button": {
+    fontFamily: "$default",
+    fontWeight: 400,
+  },
+});
