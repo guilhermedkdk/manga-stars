d1302aad3aad5b3ab8475c3cf3da49da6b20d41d
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-18 15:32:45 -0300

    feat: add sidebar component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Binoculars, ChartLineUp, SignIn, SignOut, User } from "phosphor-react";
+
+import logoImg from "@/../public/logo.svg";
+import sidebarBackground from "@/../public/sidebar.svg";
+
+import {
+  ImageWrapper,
+  LoginButton,
+  NavButton,
+  NavigationWrapper,
+  SidebarContainer,
+  TopContainer,
+} from "./styles";
+
+export default function Sidebar() {
+  const session = "authenticated";
+
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  return (
+    <SidebarContainer>
+      <Image
+        src={sidebarBackground}
+        fill={true}
+        alt={""}
+        style={{
+          objectFit: "cover",
+          overflow: "hidden",
+          borderRadius: 12,
+          zIndex: -1,
+        }}
+      />
+
+      <TopContainer>
+        <Link href={"/"}>
+          <Image src={logoImg} alt="" width={136} height={32} />
+        </Link>
+
+        <NavigationWrapper>
+          <NavButton href="/" active={currentRoute === "/home"}>
+            <ChartLineUp size={24} /> Início
+          </NavButton>
+
+          <NavButton href="/" active={currentRoute === "/explore"}>
+            <Binoculars size={24} /> Explorar
+          </NavButton>
+
+          {session === "authenticated" && (
+            <NavButton href={"/"}>
+              <User size={24} />
+              Perfil
+            </NavButton>
+          )}
+        </NavigationWrapper>
+      </TopContainer>
+
+      {session === "authenticated" ? (
+        <LoginButton href={"/"}>
+          <ImageWrapper>
+            <User size={24} />
+          </ImageWrapper>
+          Guilherme
+          <SignOut size={20} color="#F75A68" />
+        </LoginButton>
+      ) : (
+        <LoginButton href={"/"}>
+          <strong>Fazer login</strong>
+          <SignIn size={20} weight="fill" color="#50B2C0" />
+        </LoginButton>
+      )}
+    </SidebarContainer>
+  );
+}
---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,100 @@
+import Link from "next/link";
+
+import { styled } from "@/styles/stitches.config";
+
+export const SidebarContainer = styled("aside", {
+  position: "relative",
+
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "space-between",
+
+  width: "232px",
+  padding: "$10 $13 $6",
+  borderRadius: "$lg",
+
+  marginTop: "$5",
+  marginBottom: "$5",
+
+  boxShadow: "0 0 3px 1px $gray500",
+});
+
+export const TopContainer = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+
+  gap: "$16",
+});
+
+export const NavigationWrapper = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  marginInline: "auto",
+
+  gap: "$4",
+});
+
+export const NavButton = styled(Link, {
+  display: "flex",
+  alignItems: "center",
+
+  height: "42px",
+  gap: "$3",
+
+  color: "$gray400",
+
+  transition: ".2s",
+
+  "&:hover": {
+    color: "$gray100",
+  },
+
+  variants: {
+    active: {
+      true: {
+        position: "relative",
+
+        fontWeight: "$bold",
+        color: "$gray100",
+
+        "&::before": {
+          content: "",
+          position: "absolute",
+          top: "calc(50% - 24px/2)",
+          left: "-20px",
+
+          height: "24px",
+          width: "4px",
+          borderRadius: "$full",
+
+          background: "$gradient-vertical",
+        },
+      },
+    },
+  },
+});
+
+export const LoginButton = styled(Link, {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  gap: "$3",
+
+  img: {
+    borderRadius: "$full",
+  },
+});
+
+export const ImageWrapper = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  width: "34px",
+  height: "34px",
+  borderRadius: "$full",
+
+  background: "$gradient-vertical",
+});
