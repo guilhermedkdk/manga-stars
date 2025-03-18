3976efdff98ebacbbd1fe4c0cdb50448515055df
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-18 15:31:41 -0300

    feat: adjustment on global css + custom scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,10 +11,33 @@ export const globalStyles = globalCss({
     backgroundColor: "$gray800",
     color: "$gray100",
     "-webkit-font-smoothing": "antialiased",
+
+    "*::-webkit-scrollbar": {
+      width: 22,
+      height: 22,
+      borderRadius: 9999,
+    },
+
+    "*::-webkit-scrollbar-corner": {
+      backgroundColor: "transparent",
+    },
+
+    "*::-webkit-scrollbar-thumb": {
+      width: 6,
+      backgroundColor: "transparent",
+      borderRadius: 80,
+      boxShadow: "inset 0 0 0px 6px $gray500",
+      border: "10px solid transparent",
+    },
   },
 
   "body, input, textarea, button": {
     fontFamily: "$default",
     fontWeight: 400,
+  },
+
+  a: {
+    textDecoration: "none",
+    color: "inherit",
   },
 });
--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -17,6 +17,13 @@ export const { styled, globalCss, getCssText } = createStitches({
       7: "1.75rem",
       8: "2rem",
       10: "2.5rem",
+      11: "2.75rem",
+      12: "3rem",
+      13: "3.25rem",
+      14: "3.5rem",
+      16: "4rem",
+      18: "4.5rem",
+      24: "6rem",
     },
 
     fontSizes: {
@@ -71,6 +78,27 @@ export const { styled, globalCss, getCssText } = createStitches({
       md: "10px",
       lg: "20px",
       full: "99999px",
+    },
+
+    shadows: {
+      white: "#FFFFFF",
+      black: "#000000",
+
+      green100: "#50B2C0",
+      green200: "#255D6A",
+      green300: "#0A313C",
+
+      purple100: "#8381D9",
+      purple200: "#2A2879",
+
+      gray100: "#F8F9FC",
+      gray200: "#E6E8F2",
+      gray300: "#D1D6E4",
+      gray400: "#8D95AF",
+      gray500: "#303F73",
+      gray600: "#252D4A",
+      gray700: "#181C2A",
+      gray800: "#0E1116",
     },
   },
 });
