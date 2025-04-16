ba5ab272b928c0a310546b11177a6e115cfdaf33
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-04-15 21:49:31 -0300

    feat: add user registration with prisma adapter using his email

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,24 +1,62 @@
+import { NextApiRequest, NextApiResponse, NextPageContext } from "next";
 import NextAuth, { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+import GithubProvider, { GithubProfile } from "next-auth/providers/github";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
+import { PrismaAdapter } from "@/libs/auth/prisma-adapter";
+
+export function buildNextAuthOptions(
+  req: NextApiRequest | NextPageContext["req"],
+  res: NextApiResponse | NextPageContext["res"]
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+    providers: [
+      GoogleProvider({
+        allowDangerousEmailAccountLinking: true,
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+          },
         },
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+            avatar_url: profile.picture,
+          };
+        },
+      }),
+      GithubProvider({
+        allowDangerousEmailAccountLinking: true,
+        clientId: process.env.GITHUB_ID ?? "",
+        clientSecret: process.env.GITHUB_SECRET ?? "",
+        profile(profile: GithubProfile) {
+          return {
+            id: profile.id,
+            name: profile.name!,
+            email: profile.email!,
+            avatar_url: profile.avatar_url,
+          };
+        },
+      }),
+    ],
+    callbacks: {
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        };
       },
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
-};
-export default NextAuth(authOptions);
+    },
+  };
+}
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res));
+}
